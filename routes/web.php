9341ab8e00ef71291f2d608f08ea164ccc61c28d
minhquan349937 <quocphan370@gmail.com>
2025-04-13 22:50:53 +0700

    tuan 3

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\clients\HomeController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('home');
+// });
+Route::get('/', [HomeController::class, 'index'])->name('home');
